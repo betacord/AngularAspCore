27cc4afcae2ac5bd9189e56444fc5bc8f9a6c6fd
betacord <t-krzywicki@wp.pl>
2017-11-22 16:41:34 +0100

    domain models and interfaces for repositories have been added

---
+++ API/App.Core/Domain/Entity.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evento.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
